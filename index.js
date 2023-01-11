e2452164c6aef1210976c19b9304c92ad0a29030
andaru akbar <andaruakbar11@gmail.com>
2023-01-12 00:53:04 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import db from "./config/Database.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import router from "./routes/index.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+try {
+  await db.authenticate();
+  console.log("Database Connected..");
+} catch (error) {
+  console.log(error);
+}
+
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(router);
+
+app.listen(5000, () => console.log("server running at port 5000"));
